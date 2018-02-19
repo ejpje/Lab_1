f0628537c75ef81b4060c1d60486b28ccb90b54b
ejpje <emilypettit@emilysmbp12417.home>
2018-02-18 21:51:48 -0600

    Organizing javascript, adding proportional symbols to map, new data

--- js/geojson.js
+++ js/geojson.js
@@ -1,47 +1,63 @@
 /*Script by Emily Pettit, 2018*/
-/*GeoJSON Map*/
 
+//create the map
 function createMap(){
   var map = L.map('map', {
-    center: [37.0902, -95.7129],
-    zoom: 4,
+    center: [42.0000, -95.7129],
+    zoom: 3,
     minZoom: 2,
     maxZoom: 13,
-  })
+  });
 
-  var street = L.tileLayer("https://api.mapbox.com/styles/v1/ejp3/cjdku7n67019b2qpdvw0i6xr6/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWpwMyIsImEiOiJjamRrZ2g2d2EwMGoxMndxejdwd2poMGFhIn0.Ypo-SnygyDT2skpNIEQ60g", {
-    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'> Mapbox, 'North Star' style",
+//add a tileset to the map
+  L.tileLayer("https://api.mapbox.com/styles/v1/ejp3/cjdkuejlj014j2solx6e6meqf/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWpwMyIsImEiOiJjamRrZ2g2d2EwMGoxMndxejdwd2poMGFhIn0.Ypo-SnygyDT2skpNIEQ60g", {
+    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'> Streets tileset from Mapbox",
     minZoom: 2,
     maxZoom: 13,
   }) .addTo(map);
 
-    getData(map);
+  //call getData function
+  getData(map);
 };
 
-  //function to retrieve the data and place it on the map
-  function getData(map){
-      //load the data
-      $.ajax("data/natl_parks.geojson", {
-          dataType: "json",
-          success: function(response){
-
-            var geojsonMarkerOptions = {
-              radius: 8,
-              fillColor: "#006600",
-              color: "#000",
-              weight: 1,
-              opacity: 1,
-              fillOpacity: 0.8,
-            };
-
-              //create a Leaflet GeoJSON layer and add it to the map
-              L.geoJson(response, {
-                pointToLayer: function (feature, latlng){
-                  return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-            }).addTo(map);
-          }
-      });
+//function to attach popups
+function onEachFeature(feature, layer) {
+  //no property named popupContent; instead, create html string with all properties
+  var popupContent = "";
+  if (feature.properties) {
+    //loop to add feature property names and values to html string
+    for (var property in feature.properties){
+      popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+    }
+    layer.bindPopup(popupContent);
   };
+};
 
-  $(document).ready(createMap);
+//function to import GeoJSON data
+function getData(map){
+  //load the data
+  $.ajax("data/Fires.geojson", {
+    dataType: "json",
+    success: function(response){
+      //create marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+      };
+
+      //create a Leaflet GeoJSON layer and add it to the map
+      L.geoJson(response, {
+        //onEachFeature: onEachFeature
+        pointToLayer: function (feature, latlng){
+          return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+      }).addTo(map);
+    }
+  });
+};
+
+$(document).ready(createMap);
--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
 /*Script by Emily Pettit, 2018*/
+
+function calcPropRadius(attValue) {
+  //scale factor to adjust symbol size evenly
+  var scaleFactor = 50;
+  //area based on attribute value and scale factor
+  var area = attValue * scaleFactor;
+  //radius calculated based on area
+  var radius = (Math.sqrt(area/Math.PI))*(.1);
+
+  return radius;
+};
+
+//add circle markers for point features to the map
+function createPropSymbols(data, map){
+
+  var attribute = "wildFires_2017";
+
+  //create marker options
+  var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff0000",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+  };
+
+  //create a Leaflet GeoJSON layer and add it to the map
+  L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      //determine the value for each feature
+      var attValue = Number(feature.properties[attribute]);
+
+      //give each feature's circle marker a radius
+      geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+      //create circle markers
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+  }).addTo(map);
+};
+
+
+//import GeoJSON data
+function getData(map){
+  //load the data
+  $.ajax("data/Fires.geojson", {
+    dataType: "json",
+    success: function(response){
+      //call function to create proportional symbols
+      createPropSymbols(response, map);
+    }
+  });
+};
